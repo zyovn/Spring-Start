a53ffe4a832fc67cd917127b4bb39bf5b1c79d7e
zyovn <yui2507@naver.com>
2025-04-08 04:17:04 +0900

    inital commit

---
+++ src/test/java/inflearn/spring_start/SpringStartApplicationTests.java
@@ -0,0 +1,13 @@
+package inflearn.spring_start;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
