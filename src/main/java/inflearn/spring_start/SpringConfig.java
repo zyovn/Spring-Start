96f019a30a73be80ee47c7db559e45fd20b34b0c
zyovn <yui2507@naver.com>
2025-09-12 01:30:45 +0900

    Section 07: Spring JdbcTemplate

--- src/main/java/inflearn/spring_start/SpringConfig.java
+++ src/main/java/inflearn/spring_start/SpringConfig.java
@@ -1,13 +1,22 @@
 package inflearn.spring_start;
 
+import inflearn.spring_start.repository.JdbcTemplateMemberRepository;
 import inflearn.spring_start.repository.MemberRepository;
 import inflearn.spring_start.repository.MemoryMemberRepository;
 import inflearn.spring_start.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private final DataSource dataSource;
+
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,6 +25,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        // return new MemoryMemberRepository();
+        return new JdbcTemplateMemberRepository(dataSource);
     }
 }
